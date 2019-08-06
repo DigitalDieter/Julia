0de0a419e6a25e08554c7f9d58d7f95468f7ba90
DigitalDieter <devops@backupmailer.de>
2019-08-06 23:42:01 +0200

    tensorflow_test.jl added

---
+++ tensorflow_test.jl
@@ -0,0 +1,17 @@
+#!/usr/local/bin/julia
+using TensorFlow
+using Test
+
+sess = TensorFlow.Session()
+
+x = TensorFlow.constant(Float64[1,2])
+y = TensorFlow.Variable(Float64[3,4])
+z = TensorFlow.placeholder(Float64)
+
+w = exp(x + z + -y)
+
+run(sess, TensorFlow.global_variables_initializer())
+res = run(sess, w, Dict(z=>Float64[1,2]))
+@test res[1] ≈ exp(-1)
+print(res[1)
+println(res[1] ≈ exp(-1))
